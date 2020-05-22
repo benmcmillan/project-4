5fbefdb7650e41a107caf050683638ac9cb0098a
Ben McMillan <62913456+benmcmillan@users.noreply.github.com>
2020-05-21 19:42:24 -0700

    Finished Creating a To-Do App

--- client/javascripts/to-do.js
+++ client/javascripts/to-do.js
@@ -2,6 +2,7 @@
 
 let controller = function() {
 
+
   let addCommentFromInputBox = function() {
     //Semmy uses "$" to name variables that will contain jQuery objects
     let $new_comment;
@@ -12,8 +13,13 @@ let controller = function() {
       $(".comments").append($new_comment);
       //$new_comment.fadeIn();
       $(".comment-input input").val("");
+
+      //log the list of paragraph elements
+      localStorage.setItem("toDoList", $(".comments").html());
+      console.log($(".comments").html());
     }
   };
+
 
   $(".comment-input button").on("click", function(event) {
     addCommentFromInputBox();
@@ -24,6 +30,17 @@ let controller = function() {
       addCommentFromInputBox();
     }
   });
-};
+
+if (window.localStorage != "") {
+  return $(".comments").html(localStorage.getItem("toDoList")) ;
+}
+else {
+  return null;
+
+}};
 
 $(document).ready(controller);
+
+/*$(document).ready(() => {
+  //call the delete button and assign the click part the onevent handler XCCCC
+)};*/
